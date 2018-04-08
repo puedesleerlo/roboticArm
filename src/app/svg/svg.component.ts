a45f43e7582e290c7743c42556bf6c018b39c61b
Alejandro Tabares <atabaresa.unal.edu.co>
2018-04-08 15:51:44 -0500

    versión beta

--- src/app/svg/svg.component.ts
+++ src/app/svg/svg.component.ts
@@ -8,19 +8,47 @@ import { Component, OnInit, Input, OnChanges, SimpleChange  } from '@angular/cor
 export class SvgComponent implements OnInit {
   @Input() efectorX: number;
   @Input() efectorY: number;
-  @Input() poleX: number;
-  @Input() poleY: number;
+  // @Input() poleX: number;
+  // @Input() poleY: number;
   @Input() centro: number;
+  poleX:number;
+  poleY:number;
+  segmentlength= 83;
   constructor() { }
 
   ngOnInit() {
+    this.drawArm(160, 160, -1)
   }
-  // ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
-  //   this.efectorx = this.a1*Math.cos(this.theta1) + this.a2*Math.cos(this.theta1 + this.theta2)
-  //   this.efectory = this.a1*Math.sin(this.theta1) + this.a2*Math.sin(this.theta1 + this.theta2)
-  //   this.joint2x = this.a1*Math.cos(this.theta1)
-  //   this.joint2y = this.a1*Math.sin(this.theta1)
-  // }
+  ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
+    this.drawArm(this.efectorX, this.efectorY, -1)
+  }
 
+  drawArm(endEffectorX, endEffectorY, preferredRotation){
+    var dirx = endEffectorX - this.centro;
+    var diry = endEffectorY - this.centro;
+    var len = Math.sqrt(dirx * dirx + diry * diry);
+    dirx = dirx / len;
+    diry = diry / len;
+    
+    var poleX, poleY;
+    var disc = this.segmentlength * this.segmentlength - len * len / 4;
+    if(disc < 0){
+        poleX = this.centro + dirx * this.segmentlength;
+        poleY = this.centro + diry * this.segmentlength;
+        endEffectorX = this.centro + dirx * this.segmentlength * 2;
+        endEffectorY = this.centro + diry * this.segmentlength * 2;
+    } else {
+        poleX = this.centro + dirx * len / 2;
+        poleY = this.centro + diry * len / 2;
+        disc = Math.sqrt(disc);
+        if(preferredRotation < 0){
+            disc =- disc; // Make it a negative number
+        }
+        poleX -= diry * disc;
+        poleY += dirx * disc;
+    }
+    this.poleX = poleX;
+    this.poleY = poleY;
 
+  };
 }
