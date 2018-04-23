441ed5c6b22da290fff269d9104bec6c63ec0f88
Alejandro Tabares <atabaresa.unal.edu.co>
2018-04-23 04:19:11 -0500

    aún hay esperanza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  baseAngle:number = 0;
-  efectorAngle:number = 0;
   a1 = 8;
   a2 = 9;
-  centroX = 20;
-  centroY = 5;
+  centroX = 0;
+  centroY = 0;
   client: WebSocket
   messages: string[] = []
+  _baseAngle = 0
+  _efectorAngle = 0
+  _x: number = 17
+  _y: number = 0
+  efeX: number
+  efeY: number
+  togle = true
   ngOnInit() {
     this.client  = new WebSocket('wss://legoev3.mybluemix.net/ws/robot')
     this.ws(this.client);
-
+    this._baseAngle = 0
+    this._efectorAngle = 0
   }
   ws(client) {
     // Set event handlers.
@@ -49,36 +55,106 @@ export class AppComponent implements OnInit {
     // this.client.send(message)
     // this.messages.push(message)
   }
-  get x() {
-    return this.a1*Math.cos(this.toRadians(this.baseAngle)) 
-    + this.a2*Math.cos(this.toRadians(this.baseAngle + this.efectorAngle)) + this.centroX;
+  get status() {
+    return this.togle ? "Codo arriba" : "Codo abajo"
   }
+  get togleNumber() {
+    let n = this.togle ? 1 : -1
+    // this._baseAngle = n*this._baseAngle
+    // this._efectorAngle = n*this._efectorAngle
+    return n
+  }
+  get x() {
+    let x = this.a1*Math.cos(this.toRadians(this._baseAngle)) 
+    + this.a2*Math.cos(this.toRadians(this._baseAngle + this._efectorAngle)) + this.centroX;
+    
+    return x
+  }
+  get y() {
+    let y = this.a1*Math.sin(this.toRadians(this._baseAngle)) 
+    + this.a2*Math.sin(this.toRadians(this._baseAngle + this._efectorAngle)) + this.centroY;
+    return y
+   }
   get poleX() {
-    var angle = this.toRadians(this.baseAngle)
+    var angle = this.toRadians(this._baseAngle)
     return this.a1*Math.cos(angle) + this.centroX
   }
   get poleY() {
-    var angle = this.toRadians(this.baseAngle)
+    var angle = this.toRadians(this._baseAngle)
+    
     return this.a1*Math.sin(angle) + this.centroY
-  }
-  get y() {
-   return this.a1*Math.sin(this.toRadians(this.baseAngle)) 
-   + this.a2*Math.sin(this.toRadians(this.baseAngle + this.efectorAngle)) + this.centroY;
   }
   toRadians (angle) {
     return angle * (Math.PI / 180);
   }
+  set x(val) {
+    this._x = val
+    this._baseAngle = this.baseAngle
+    this._efectorAngle = this.efectorAngle
+  }
+  set y(val) {
+    this._y = val
+    this._baseAngle = this.baseAngle
+    this._efectorAngle = this.efectorAngle
+
+  }
+  get efectorAngle() {
+    let theta2 = Math.pow(this._x,2) + Math.pow(this._y,2) - Math.pow(this.a1,2) - Math.pow(this.a2,2)
+    theta2 = theta2/(2*this.a1*this.a2)
+    theta2 =  Math.acos(theta2)
+    if(!theta2) theta2 = 0
+    let angle = this.toDegrees(this.togleNumber*theta2)
+    return angle
+  }
+  get baseAngle() {
+    let distance = Math.pow(this._x,2) + Math.pow(this._y,2)
+    let alpha = distance + Math.pow(this.a1,2) - Math.pow(this.a2,2)
+    let atan = Math.atan(this._y/this._x)
+    let acos = Math.acos(alpha/(2*this.a1*Math.sqrt(distance)))
+    if(!acos) acos = 0
+    let theta1 = atan - this.togleNumber*acos
+    let angle = this.toDegrees(theta1)
+    return angle
+  }
+  set efectorAngle(val) {
+    this._efectorAngle = val
+    // this._x = this.x
+    // this._y = this.y
+  }
+  set baseAngle(val) {
+    this._baseAngle = val
+    // this._x = this.x
+    // this._y = this.y
+  }
+  xRect(time) {
+    var xrec = setInterval(() => {
+      console.log("d")
+      this.x = this.x - 1
+      if(this.x < 4) clearInterval(xrec)   
+    }, time)
+  }
+  yRect(time) {
+    var yrec = setInterval(() => {
+      console.log("d")
+      this.y = this.y - 1
+      if(this.y < -10) clearInterval(yrec)   
+    }, time)
+  }
+  yequalx(time) {
+    var yx = setInterval(() => {
+      console.log("d")
+      this.x = this.x - 1
+      this.y = this.x
+      if(this.x < 4) clearInterval(yx)   
+    }, time)
+  }
+  reset() {
+    this.x = this.a1 + this.a2
+    this.y = 0
+  }
+
   toDegrees (angle) {
     return angle * (180 / Math.PI);
   }
-  // inverseKinematics() {
-  //   let positionX = this.positionX- this.centro;
-  //   let positionY = this.positionY- this.centro;
-  //   let theta2 = Math.pow(positionX,2) + Math.pow(positionY,2) - Math.pow(this.segmentlength,2) - Math.pow(this.a2,2)
-  //   theta2 = theta2/(2*this.segmentlength*this.segmentlength)
-  //   theta2 = Math.acos(theta2)
-  //   let theta1 = Math.atan(positionY/positionX) - Math.atan((this.segmentlength*Math.sin(theta2))/(this.segmentlength + this.segmentlength*Math.cos(theta2)))
-  //   this.baseAngle = this.toDegrees(theta1)
-  //   this.efectorAngle = this.toDegrees(theta2)
-  // }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { SvgComponent } from './svg/svg.component';
 import { MultiplierPipe } from './multiplier.pipe';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -15,7 +22,14 @@ import { MultiplierPipe } from './multiplier.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
