cfca2003275e21b251266e9e994282a8f23f9d3e
Andrea Giraldo <yagiraldot@outlook.es>
2018-04-26 16:30:15 -0500

    mejorada movilidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  a1 = 8;
-  a2 = 9;
+  a1 = 6.5;
+  a2 = 12.7;
   centroX = 0;
   centroY = 0;
   client: WebSocket
   messages: string[] = []
   _baseAngle = 0
   _efectorAngle = 0
-  _x: number = 17
+  _x: number = 19.2
   _y: number = 0
   efeX: number
   efeY: number
   togle = true
+  q1f: number
+  q2f: number
+  x2f: number
+  y2f: number
   ngOnInit() {
     this.client  = new WebSocket('wss://legoev3.mybluemix.net/ws/robot')
     this.ws(this.client);
@@ -108,6 +112,7 @@ export class AppComponent implements OnInit {
     return angle
   }
   get baseAngle() {
+    
     let distance = Math.pow(this._x,2) + Math.pow(this._y,2)
     let alpha = distance + Math.pow(this.a1,2) - Math.pow(this.a2,2)
     let atan = Math.atan(this._y/this._x)
@@ -115,6 +120,7 @@ export class AppComponent implements OnInit {
     if(!acos) acos = 0
     let theta1 = atan - this.togleNumber*acos
     let angle = this.toDegrees(theta1)
+    
     return angle
   }
   set efectorAngle(val) {
@@ -128,52 +134,79 @@ export class AppComponent implements OnInit {
     // this._y = this.y
   }
   xRect(time) {
-    var xrec = setInterval(() => {
-
-      this.x = this.x - 1
-      var message = this.returnMessage()
-      this.client.send(message)
-      if(this.x < 4) clearInterval(xrec)   
-    }, time)
+    var velocidad = 0
+    var coeficientes = this.coeficientesAngulo(this._x, this.x2f, 0, 0, time)
+    var coeficientesBase = this.coeficientesAngulo(this._y, this.y2f, 0, 0, time)
+    var yx = setInterval(() => {
+      this.x = this.polinomiosAngulo(velocidad, coeficientes)
+      this.y = this.polinomiosAngulo(velocidad, coeficientesBase)
+      velocidad++
+      console.log(this._x)
+      this.sendMessage(this.baseAngle, this.efectorAngle)
+      if(velocidad == time) clearInterval(yx)
+    }, 100) 
   }
   yRect(time) {
-    var yrec = setInterval(() => {
- 
-      this.y = this.y - 1
-      var message = this.returnMessage()
-      this.client.send(message)
-      if(this.y < -10) clearInterval(yrec)   
-    }, time)
+    this.x = this.x2f
+    this.y = this.y2f
+    this.sendMessage(this.baseAngle, this.efectorAngle)
   }
   yequalx(time) {
-    var yx = setInterval(() => {
+    // var yx = setInterval(() => {
 
-      this.x = this.x - 1
-      this.y = this.x
-      var message = this.returnMessage()
-      this.client.send(message)
-      if(this.x < 4) clearInterval(yx)   
-    }, time)
+    //   this.x = this.x - 1
+    //   this.y = this.x
+    //   var message = this.returnMessage()
+    //   this.client.send(message)
+    //   if(this.y < 5) clearInterval(yx)   
+    // }, time)
   }
   angleTrajectory(time) {
+    var velocidad = 0
+    var coeficientes = this.coeficientesAngulo(this._baseAngle, this.q2f, 0, 0, time)
+    var coeficientesBase = this.coeficientesAngulo(this._efectorAngle, this.q1f, 0, 0, time)
     var yx = setInterval(() => {
+      this.efectorAngle = this.polinomiosAngulo(velocidad, coeficientes)
+      this.baseAngle = this.polinomiosAngulo(velocidad, coeficientesBase)
+      velocidad++;
 
-      this.efectorAngle = this.efectorAngle + 1
-      this.baseAngle = this.baseAngle + 1
-      var message = this.returnMessage()
-      this.client.send(message)
-      if(this.efectorAngle > 120) clearInterval(yx) 
-      if(this.baseAngle > 120) clearInterval(yx)  
-    }, time)
+      this.sendMessage(this._baseAngle, this._efectorAngle)
+      if(velocidad == time) clearInterval(yx)
+    }, 100)      
+
+  }
+  coeficientesAngulo(qi, qf, qdi, qdf, tf){//qdi= ad de qi
+   //con ti = 0
+    var a0 = qi
+    var a1 = qdi
+    var a2 = (-3*(qi-qf) - (2*qdi+qdf)*tf)/(tf*tf)
+    var a3 = (2*(qi-qf)+(qdi+qdf)*tf)/(tf*tf*tf)
+    return {a0, a1, a2, a3}
+
   }
 
-  returnMessage() {
-    return '{"move": {"baseAngle": ' + this.baseAngle + ', "efectorAngle":' + this.efectorAngle + ' }}'
+  polinomiosAngulo(t, coeficientes){
+    var n = coeficientes
+    console.log(coeficientes)
+    var q = n.a0 + n.a1*t + n.a2*t*t + n.a3*t*t*t
+    var qvel = n.a1 + 2*n.a2*t + 3*n.a3*t*t
+    return q
+  }
+  // valoresTetha(){
+
+  sendMessage(a, b) {
+    var message = this.returnMessage(a, b)
+    this.client.send(message)
+  }
+  // }
+  returnMessage(a, b) {
+    return '{"move": {"baseAngle": ' + a + ', "efectorAngle":' + b + ' }}'
   }
   
   reset() {
     this.x = this.a1 + this.a2
     this.y = 0
+    this.sendMessage(this.baseAngle, this.efectorAngle)
   }
 
   toDegrees (angle) {
